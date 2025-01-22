99cca1dce04ac3655df0667844c684d5597573c8
Jamsher <nodirovic17@gmail.com>
2025-01-22 20:09:54 +0500

    DB Setup

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'monitoring_db',
+        'USER': 'postgres',
+        'PASSWORD': 'VL2eiGD8mTAckE3PzEdj',
+        'HOST': 'monitoring-db.cb6iags40m0f.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
